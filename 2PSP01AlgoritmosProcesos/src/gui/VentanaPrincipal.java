d1e9622667aa61a0b5686640dd5d16c3c96126d4
Kailneg <cuenta_buzon@hotmail.com>
2017-11-03 11:16:07 +0100

    Creado paquete gui y clase ventana principal

--- 2PSP01AlgoritmosProcesos/src/gui/VentanaPrincipal.java
+++ 2PSP01AlgoritmosProcesos/src/gui/VentanaPrincipal.java
@@ -31,6 +31,7 @@ public class VentanaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public VentanaPrincipal() {
+		setTitle("Planificador de Procesos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
